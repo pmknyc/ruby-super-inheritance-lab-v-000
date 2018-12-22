8f7a8bd5ecb9a5729669d3a6d812a2d21db60954
Paula McKinley <pmknyc@optimum.net>
2018-12-22 23:37:31 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,8 +1,7 @@
 class ChattyStudent < Student
 
   def hello
-    super
-    @hello
+    super.hello
     puts "How are you doing today? I'm okay, but I'm kind of tired.
         Did you watch The Walking Dead last night? You didn't?!
         Oh man, it was so crazy! What, you don't want any spoilers?
