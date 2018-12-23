19b2a247deac127332df16c141a22d8682e0ddd5
Paula McKinley <pmknyc@optimum.net>
2018-12-23 00:24:18 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,7 +3,7 @@ class ChattyStudent < Student
 
   def hello
     super
-    #puts "How are you doing today? I'm okay, but I'm kind of tired.\n Did you watch The Walking Dead last night? You didn't?! \n Oh man, it was so crazy! What, you don't want any spoilers? \n Okay well let me just tell you who died..."
+    puts "How are you doing today? I'm okay, but I'm kind of tired.\n Did you watch The Walking Dead last night? You didn't?! \n Oh man, it was so crazy! What, you don't want any spoilers? \n Okay well let me just tell you who died..."
   end
   binding.pry
 
