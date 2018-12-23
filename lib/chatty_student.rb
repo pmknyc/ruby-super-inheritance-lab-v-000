ebc0c14b2a48865216e324bc7a099e6585f2213d
Paula McKinley <pmknyc@optimum.net>
2018-12-23 00:56:32 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,7 +2,8 @@ require 'pry'
 class ChattyStudent < Student
 
   def hello
-    puts "#{super} \nHow are you doing today?
+    super
+    puts "How are you doing today?
     I'm okay, but I'm kind of tired.
     Did you watch The Walking Dead last night? You didn't?!
     Oh man, it was so crazy! What, you don't want any spoilers?
